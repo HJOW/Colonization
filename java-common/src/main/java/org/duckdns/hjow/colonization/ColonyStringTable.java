892bc97b5f7db6ea7c73d30ebe8d158d372a3598
HJOW <hujinone22@naver.com>
2025-10-11 16:38:38 +0900

    다국어 지원 스트링 테이블에 텍스트가 없는 경우를 대비

--- java-common/src/main/java/org/duckdns/hjow/colonization/ColonyStringTable.java
+++ java-common/src/main/java/org/duckdns/hjow/colonization/ColonyStringTable.java
@@ -2,6 +2,7 @@ package org.duckdns.hjow.colonization;
 
 import java.util.Properties;
 
+import org.duckdns.hjow.commons.resource.FileStringTable;
 import org.duckdns.hjow.commons.resource.StringTable;
 
 /** Colonization 다국어 구현을 위한 스트링 테이블 */
@@ -11,7 +12,13 @@ public class ColonyStringTable implements StringTable {
 
 	@Override
 	public String t(String originals) {
-		if(originalInstance == null) return originals;
+		if(originalInstance == null) {
+			if(originalInstance instanceof FileStringTable) {
+				// TODO : 공통lib 업데이트 후, 대체 메소드로 변경
+				originalInstance.getData().setProperty(originals, originals);
+			}
+			return originals;
+		}
 		return originalInstance.t(originals);
 	}
 
