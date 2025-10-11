717137b71b7d7b6ff3456dfd8d694bb57162a151
HJOW <hujinone22@naver.com>
2025-10-11 17:37:16 +0900

    공통 lib 업데이트, 다국어 지원 테이블 미비 텍스트의 케이스 추가구현

--- java-common/src/main/java/org/duckdns/hjow/colonization/ColonyStringTable.java
+++ java-common/src/main/java/org/duckdns/hjow/colonization/ColonyStringTable.java
@@ -2,43 +2,42 @@ package org.duckdns.hjow.colonization;
 
 import java.util.Properties;
 
+import org.duckdns.hjow.commons.resource.BrokerStringTable;
 import org.duckdns.hjow.commons.resource.FileStringTable;
 import org.duckdns.hjow.commons.resource.StringTable;
 
 /** Colonization 다국어 구현을 위한 스트링 테이블 */
-public class ColonyStringTable implements StringTable {
+public class ColonyStringTable extends BrokerStringTable {
 	private static final long serialVersionUID = -1072075853155908566L;
-	protected StringTable originalInstance;
+	
+	public ColonyStringTable() { super(); }
+	public ColonyStringTable(StringTable stringTable) { super(stringTable); }
+	public ColonyStringTable(String name, StringTable stringTable) { super(name, stringTable); }
 
 	@Override
 	public String t(String originals) {
-		if(originalInstance == null) {
-			if(originalInstance instanceof FileStringTable) {
-				// TODO : 공통lib 업데이트 후, 대체 메소드로 변경
-				originalInstance.getData().setProperty(originals, originals);
+		Properties prop = originalInstance.getData();
+		String res = null;
+		if(prop.containsKey(originals)) res = prop.getProperty(originals);
+		
+		if(res == null) {
+			if(originalInstance != null) {
+				if(originalInstance instanceof FileStringTable) {
+					FileStringTable fileTable = (FileStringTable) originalInstance;
+					fileTable.set(originals, originals, true);
+				} else {
+					originalInstance.getData().setProperty(originals, originals);
+				}
+				return originals;
 			}
-			return originals;
 		}
-		return originalInstance.t(originals);
+		
+		return res;
 	}
 
 	@Override
 	public String getName() {
 		if(originalInstance == null) return "ColonizationStringTable";
 		return originalInstance.getName();
-	}
-
-	@Override
-	public Properties getData() {
-		if(originalInstance == null) return new Properties();
-		return originalInstance.getData();
-	}
-
-	public StringTable getOriginalInstance() {
-		return originalInstance;
-	}
-
-	public void setOriginalInstance(StringTable originalInstance) {
-		this.originalInstance = originalInstance;
 	}
 }
