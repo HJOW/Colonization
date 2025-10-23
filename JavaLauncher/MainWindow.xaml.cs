2ad7c808be25266f0345f247e960c178bda051c1
HJOW <hjow@djict.co.kr>
2025-10-23 16:40:28 +0900

    닷넷기반 런처 프로그램 개발 시작

---
+++ JavaLauncher/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JavaLauncher
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string ROOTPATH = Util.GetUserHomePath() + System.IO.Path.DirectorySeparatorChar + ".colonization";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            // 디렉토리 없으면 생성
+            if (!System.IO.Directory.Exists(ROOTPATH))
+            {
+                System.IO.Directory.CreateDirectory(ROOTPATH);
+            }
+
+
+            // TODO
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Close();
+            Application.Current.Shutdown();
+        }
+
+        private void BtnRun_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+    }
+}
---
+++ JavaLauncher/Util.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JavaLauncher
+{
+    public class Util
+    {
+        /** null 이면 true 반환, 그외의 경우 문자열 형변환 후 TRIM, 빈 문자열인 경우 true 그외에는 false */
+        public static bool IsEmpty(object obj)
+        {
+            if (obj == null) return true;
+            string str = obj.ToString().Trim();
+            if (str.Equals("")) return true;
+            return false;
+        }
+
+        /** 어떤 객체를 bool 타입으로 변환, 변환 실패 시 예외 */
+        public static bool ParseBool(object obj)
+        {
+            if (obj == null) return false;
+            if (obj is bool) return (bool) obj;
+            string str = obj.ToString().Trim().ToLower();
+            if (str.Equals("y") || str.Equals("yes") || str.Equals("true") || str.Equals("t")) return true;
+            if (str.Equals("n") || str.Equals("no") || str.Equals("false") || str.Equals("f")) return false;
+
+            throw new Exception("Wrong boolean value " + obj);
+        }
+
+        /** 실행 파일의 위치 경로를 반환 (DLL 라이브러리로 빠지면 DLL 파일의 경로를 리턴할 수 있으니 주의 !) */
+        public static string GetThisExePath()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly().Location;
+        }
+
+        /** 사용자 홈 폴더 경로 반환 */
+        public static string GetUserHomePath()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        }
+    }
+}
