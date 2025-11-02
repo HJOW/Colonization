a72d717d0d3e28e04cc2e89a40d27c357752e56a
HJOW <hujinone22@naver.com>
2025-11-02 21:27:26 +0900

    윈도우용 배치 파일 작성

---
+++ swing/launchScripts/run.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+REM Check JAVA_HOME is declared and version is least 1.8
+IF NOT DEFINED JAVA_HOME (
+    echo [ERROR] There is no JAVA_HOME environment !
+    goto :eof
+)
+
+IF NOT EXIST "%JAVA_HOME%\bin\java.exe" (
+    echo [ERROR] Incorrect JAVA_HOME environment !
+    goto :eof
+)
+
+"%JAVA_HOME%\bin\java.exe" -jar "colonization-swing-0.0.1.jar" -cp "%HOMEDRIVE%%HOMEPATH%\lib\*" --updator Y %*
+
+:EOF
+endlocal
