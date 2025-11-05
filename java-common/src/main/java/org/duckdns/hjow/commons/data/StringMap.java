80be115dbc9a4fe5cde1f23d06cd96edb789fc56
HJOW <hjow@djict.co.kr>
2025-11-06 08:50:24 +0900

    공통 lib 다시 업그레이드

--- java-common/src/main/java/org/duckdns/hjow/commons/data/StringMap.java
+++
@@ -1,8 +0,0 @@
-package org.duckdns.hjow.commons.data;
-
-/** Map<String, String> 대안 클래스, Kotlin 과 사용 시 ClassFormatError 방지 용도 */
-public class StringMap extends java.util.HashMap<String, String> {
-	private static final long serialVersionUID = -9209451496273321544L;
-	public StringMap() { super(); }
-    
-}
