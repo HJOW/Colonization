b6905892812acb072f6328ab6396e5c57351d9d1
HJOW <hujinone22@naver.com>
2025-11-20 20:31:38 +0900

    공통 lib 업데이트, 일부 컨텐츠 공통 lib 것으로 교체

--- swing/src/main/java/org/duckdns/hjow/colonization/ui/SwingSpacePanel.java
+++ swing/src/main/java/org/duckdns/hjow/colonization/ui/SwingSpacePanel.java
@@ -16,6 +16,7 @@ import org.duckdns.hjow.commons.ui.graphics.AdvancedCoordinate3D;
 import org.duckdns.hjow.commons.ui.graphics.Coordinate2D;
 import org.duckdns.hjow.commons.ui.graphics.LineObject2D;
 import org.duckdns.hjow.commons.ui.graphics.OvalObject2D;
+import org.duckdns.hjow.commons.ui.graphics.TextObject2D;
 
 /** 함선 위치 현황 출력을 위한 패널 - 기본 Swing 의 Graphics 2D 사용 */ // TODO SampleJavaCodes 에 있는 Space3D 예제 참고하여 yaw, pitch (카메라의 방향) 개념 적용
 public class SwingSpacePanel extends SpacePanel {
